a3d738a794bbe6780b22b0c377565cd1fb066e3b
Marius Malvik <mariusmalvik@gmail.com>
2018-03-02 17:10:59 +0100

    Add log function for testing stuff out

---
+++ src/TemperatureFunctions/Test/LogFunction.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace TemperatureFunctions.Test
+{
+    public static class LogFunction
+    {
+        [FunctionName("LogFunction")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        {
+            try
+            {
+                log.Info("C# HTTP trigger function processed a request.");
+
+                if (req.Content != null)
+                {
+                    log.Info("Data sent: ", req.Content.ToString());
+                }
+
+                return new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent("{logging}", Encoding.UTF8, "application/json")
+                };
+            }
+            catch (Exception e)
+            {
+                log.Error("Function failed", e);
+                return new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            }
+        }
+    }
+}
